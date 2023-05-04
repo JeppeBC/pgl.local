9eca4227014542bc3dbaf31adc7d65d640cf8b6f
Andreas691667 <andreaskaag@hotmail.com>
2023-05-04 09:35:28 +0200

    checking if data exists

--- db.php
+++ db.php
@@ -17,35 +17,39 @@ session_start();    //start session
 echo (file_get_contents('templates/main_template.html'));
 ?>
 
-  <!--Body design-->
-  <div class="homebody">
-    <h2>Database</h2>
-    <p>Here you can find all the information about residents </p>
-  </div>
+<!--Body design-->
+<div class="homebody">
+  <h2>Database</h2>
+  <p>Here you can find all the information about residents </p>
+</div>
 
-  <?php
-  $clientId = $_SESSION['clientId']; // access session variable updated at login
 
-  if ($clientId != null) {
+<?php
+$clientId = $_SESSION['clientId']; // access session variable updated at login
 
-    // MQTT connection settings
-    $connectionSettings = (new ConnectionSettings)
-      ->setKeepAliveInterval(1) // keep alive ping interval in seconds
-      ->setConnectTimeout(3); // connection timeout in seconds
+if ($clientId != null) {
 
-    //initialize the MQTT client
-    $mqtt = new MqttClient($hostname, $port, $clientId);
+  // MQTT connection settings
+  $connectionSettings = (new ConnectionSettings)
+    ->setKeepAliveInterval(1) // keep alive ping interval in seconds
+    ->setConnectTimeout(3); // connection timeout in seconds
 
-    // Get emergency Data and present them in a table
-    try {
-      $mqtt->connect($connectionSettings, $cleanSession);
+  //initialize the MQTT client
+  $mqtt = new MqttClient($hostname, $port, $clientId);
 
-      $mqtt->subscribe(
-        $RESPONSE_EMERGENCY_TOPIC . '/' . $clientId . '/response',
-        function (string $topic, string $message) use ($mqtt, $RESPONSE_EMERGENCY_TOPIC, $clientId) { // callback function
-          // get data from the message in json format
-          $data = json_decode($message, true);
+  // Get emergency Data and present them in a table
+  try {
+    $mqtt->connect($connectionSettings, $cleanSession);
 
+    $mqtt->subscribe(
+      $RESPONSE_EMERGENCY_TOPIC . '/' . $clientId . '/response',
+      function (string $topic, string $message) use ($mqtt, $RESPONSE_EMERGENCY_TOPIC, $clientId) { // callback function
+        // get data from the message in json format
+        $data = json_decode($message, true);
+
+        if (count($data) == 0) {
+          echo '<p style="float: right">No emergencies found</p>';
+        } else {
           // Generate the HTML table code
           echo '<table style="float: right">';
           echo '<thead><tr><th>Emergency ID</th><th>Date</th><th>Emergency</th><th>Device ID</th><th>User ID</th></tr></thead>';
@@ -62,29 +66,33 @@ echo (file_get_contents('templates/main_template.html'));
           }
           echo '</tbody>';
           echo '</table>';
+        }
 
-          $mqtt->unsubscribe($RESPONSE_EMERGENCY_TOPIC . '/' . $clientId . '/response'); // unsubscribe from the topic
-          $mqtt->disconnect();  // disconnect from the broker
-        },
-        0
-      );
+        $mqtt->unsubscribe($RESPONSE_EMERGENCY_TOPIC . '/' . $clientId . '/response'); // unsubscribe from the topic
+        $mqtt->disconnect();  // disconnect from the broker
+      },
+      0
+    );
 
-      $mqtt->publish($REQUEST_GET_EMERGENCIES_TOPIC, $clientId . ';', 0, true);
+    $mqtt->publish($REQUEST_GET_EMERGENCIES_TOPIC, $clientId . ';', 0, true);
 
-      $mqtt->loop(true);
-    } catch (Exception $e) {
-    }
+    $mqtt->loop(true);
+  } catch (Exception $e) {
+  }
 
-    // Get journey Data
-    try {
+  // Get journey Data
+  try {
 
-      $mqtt->connect($connectionSettings, $cleanSession);
+    $mqtt->connect($connectionSettings, $cleanSession);
 
-      $mqtt->subscribe(
-        $RESPONSE_SEND_EVENTS_TOPIC . '/' . $clientId . '/response',
-        function (string $topic, string $message) use ($mqtt, $RESPONSE_SEND_EVENTS_TOPIC, $clientId) {
-          $data = json_decode($message, true);    //decode json data into an array
+    $mqtt->subscribe(
+      $RESPONSE_SEND_EVENTS_TOPIC . '/' . $clientId . '/response',
+      function (string $topic, string $message) use ($mqtt, $RESPONSE_SEND_EVENTS_TOPIC, $clientId) {
+        $data = json_decode($message, true);    //decode json data into an array
 
+        if (count($data) == 0) {
+          echo '<p style="float: left">No journeys found</p>';
+        } else {
           // Generate the HTML table code
           echo '<table style="float: left">';
           echo '<thead><tr><th>Emergency ID</th><th>Date</th><th>Emergency</th><th>Device ID</th><th>User ID</th></tr></thead>';
@@ -101,25 +109,24 @@ echo (file_get_contents('templates/main_template.html'));
           }
           echo '</tbody>';
           echo '</table>';
+        }
 
-          $mqtt->unsubscribe($RESPONSE_SEND_EVENTS_TOPIC . '/' . $clientId . '/response');
-          $mqtt->disconnect();
-        },
-        0
-      );
+        $mqtt->unsubscribe($RESPONSE_SEND_EVENTS_TOPIC . '/' . $clientId . '/response');
+        $mqtt->disconnect();
+      },
+      0
+    );
 
-      $mqtt->publish($REQUEST_GET_EVENTS_TOPIC, $clientId . ';', 0, true);
+    $mqtt->publish($REQUEST_GET_EVENTS_TOPIC, $clientId . ';', 0, true);
 
-      $mqtt->loop(true);
-    } catch (Exception $e) {
-    }
+    $mqtt->loop(true);
+  } catch (Exception $e) {
   }
+} else {
+  echo '<p style="text-align: center">Please log in to see the data</p>';
+}
 
-  else {
-    echo '<p style="text-align: center">Please log in to see the data</p>';
-  }
-
-  ?>
+?>
 
 
 </body>
